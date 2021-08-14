f0f1111a440ea592d2f39a1112dcc6fb2105da85
Andrei Kardovich <akardovich@gmail.com>
2021-08-14 10:28:29 +0300

    11_Server side rendering

---
+++ layout/Layout.tsx
@@ -0,0 +1,35 @@
+import { LayoutProps } from "./Layout.props";
+import styles from './Layout.module.css';
+import cn from 'classnames';
+import { Header } from "./Header/Header";
+import { Sidebar } from "./Sidebar/Sidebar";
+import { Footer } from "./Footer/Footer";
+import React,{ FunctionComponent } from "react";
+
+export const Layout = ({ children }: LayoutProps): JSX.Element => {
+  return (
+    <div className={styles.wrapper}>
+      <Header className={styles.header} />
+      <Sidebar className={styles.sidebar} />
+      <div className={styles.body}>
+        {children}
+      </div>
+      <Footer className={styles.footer}/>
+    </div>
+  );
+};
+
+
+//HOC 'Higher-Order Component'
+export const withLayout = <T extends Record<strng, unknown>> (Component: FunctionComponent<T> ) => {
+  return function withLayoutComponent(props: T): JSX.Element {
+    return (
+      <Layout>
+        <Component {...props} />
+      </Layout>
+
+    );
+  };
+};
+
+
