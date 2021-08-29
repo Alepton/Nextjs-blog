9e8e2f790447cc72cd8dfa369e99e30208c7004a
Andrei Kardovich <akardovich@gmail.com>
2021-08-29 09:28:43 +0300

    15.3_Component_Search

---
+++ components/Search/Search.tsx
@@ -0,0 +1,54 @@
+import { SearchProps } from "./Search.props";
+import styles from './Search.module.css';
+import GlassIcon from './glass.svg';
+import cn from 'classnames';
+import { Input } from "../Input/Input";
+import { Button } from "../Button/Button";
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+export const Search =({className, ...props}: SearchProps): JSX.Element => {
+  const [search, setSearch] = useState<string>('');
+  const router = useRouter();
+
+  const goToSearch = () => {
+    router.push({
+      pathname: '/search',
+      query: {
+        q: search
+      }
+
+    });
+
+  };
+
+  const handlKeyDown = (e: KeyboardEvent) => {
+    if (e.key == 'Enter') {
+      goToSearch();
+    }
+  };
+
+  return (
+    <div className={cn(className, styles.search)} {...props}>
+      <Input
+        className={styles.input}
+        placeholder='Поиск...'
+        value={search}
+        onChange={(e) => setSearch(e.target.value) }
+        onKeyDown={handlKeyDown}
+
+      />
+      <Button
+          appearance="primary"
+          className={styles.button}
+          onClick={goToSearch}
+      >
+        <GlassIcon />
+
+      </Button>
+
+    </div>
+
+  );
+
+};
