537d16d44cbdfaaefaa1e6738485cf5622ec62b9
Andrei Kardovich <akardovich@gmail.com>
2021-09-05 12:08:10 +0300

    16_1 Component_Review

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -8,10 +8,15 @@ import { Button } from "../Button/Button";
 import { declOfNum, priceRu } from "../../helpers/helpers";
 import { Divider } from "../Divider/Divider";
 import Image from 'next/image';
+import { useState } from "react";
+import { Review } from "../Review/Review";
 
 export const Product =({ product, className, ...props}: ProductProps): JSX.Element => {
+  const [isReviewOpened, setIsReviewOpened] = useState<boolean>(false);
+
   return (
-    <Card className={styles.product}>
+    <>
+      <Card className={styles.product}>
       <div className={styles.logo}>
       <Image
           src={process.env.NEXT_PUBLIC_DOMAIN + product.image}
@@ -40,7 +45,7 @@ export const Product =({ product, className, ...props}: ProductProps): JSX.Eleme
       </div>
       <div className={styles.priceTitle}>цена</div>
       <div className={styles.creditTitle}>в кредит</div>
-      <div className={styles.reteTitle}>{product.reviewCount} {declOfNum(product.reviewCount, ['отзыв', 'отзыва', 'отзывов'])}</div>
+      <div className={styles.rateTitle}>{product.reviewCount} {declOfNum(product.reviewCount, ['отзыв', 'отзыва', 'отзывов'])}</div>
       <Divider className={styles.hr} />
       <div className={styles.description}>{product.description}</div>
       <div className={styles.feature}>
@@ -62,14 +67,27 @@ export const Product =({ product, className, ...props}: ProductProps): JSX.Eleme
           <div>{product.disadvantages}</div>
         </div>}
       </div>
-      <Divider className={styles.hr} />
+      <Divider className={styles.hr, styles.hr2} />
       <div className={styles.actions}>
         <Button appearance="primary">Узнать подробнее</Button>
-        <Button appearance="ghost" arrow={"right"} className={styles.reviewButton} >
-          Читать отзывы
+        <Button
+            appearance="ghost"
+            arrow={isReviewOpened ? 'down' : 'right'}
+            className={styles.reviewButton}
+            onClick={() => setIsReviewOpened(!isReviewOpened)}
+          >Читать отзывы
         </Button>
       </div>
     </Card>
+      <Card color='blue' className={cn(styles.reviews, {
+        [styles.opend]: isReviewOpened,
+        [styles.closed]: !isReviewOpened,
+      })}>
+        {product.reviews.map(r => (
+          <Review key={r._id} review={r} />
+        ))}
+      </Card>
+    </>
   );
 
 };
