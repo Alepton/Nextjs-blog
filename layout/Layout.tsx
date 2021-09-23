9efe54fb9c954cbb055071c4c9fd238290e440e3
Andrei Kardovich <akardovich@gmail.com>
2021-09-23 20:31:12 +0300

    '17.06_useAnimation'

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -6,6 +6,8 @@ import { Sidebar } from "./Sidebar/Sidebar";
 import { Footer } from "./Footer/Footer";
 import React,{ FunctionComponent } from "react";
 import { AppContextProvider, IAppContext } from "../context/app.context";
+import { Up } from "../components";
+
 
 export const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
@@ -16,6 +18,7 @@ export const Layout = ({ children }: LayoutProps): JSX.Element => {
         {children}
       </div>
       <Footer className={styles.footer}/>
+      <Up />
     </div>
   );
 };
