e26afb98ee1a8c4b5fc35554fb0269eb7a3f20af
Andrei Kardovich <akardovich@gmail.com>
2021-10-10 10:22:14 +0300

    '18.09_Aria-hidden'

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -45,19 +45,22 @@ export const Product = motion(forwardRef(({ product, className, ...props}: Produ
         </div>
         <div className={styles.title}> {product.title} </div>
         <div className={styles.price}>
-          {priceRu(product.price)}
+          <span> <span className="visualyHidden">цена</span>{priceRu(product.price)}</span>
           {product.oldPrice && (
             <Tag className={styles.oldPrice} color="green">
+              <span className="visualyHidden">скидка</span>
               {priceRu(product.price - product.oldPrice)}
             </Tag>
           )}
         </div>
         <div className={styles.credit}>
+        <span className="visualyHidden">кредит</span>
           {" "}
           {priceRu(product.credit)}/<span className={styles.mounth}>мес</span>
         </div>
         <div className={styles.rating}>
           {" "}
+          <span className="visualyHidden">{'рейтинг' + (product.reviewAvg ?? product.initialRating)} </span>
           <Rating rating={product.reviewAvg ?? product.initialRating} />{" "}
         </div>
         <div className={styles.tags}>
@@ -68,8 +71,8 @@ export const Product = motion(forwardRef(({ product, className, ...props}: Produ
             </Tag>
           ))}{" "}
         </div>
-        <div className={styles.priceTitle}>цена</div>
-        <div className={styles.creditTitle}>в кредит</div>
+        <div className={styles.priceTitle} aria-hidden={true}>цена</div>
+        <div className={styles.creditTitle} aria-hidden={true}>в кредит</div>
         <div className={styles.rateTitle}>
           <a href="#ref" onClick={scrollToReview}>
             {product.reviewCount}{" "}
